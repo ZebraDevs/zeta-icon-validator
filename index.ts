5b31c462a1c05f118b92f0f5e61178b4223b88a3
mikecoomber <mike.coomber@zebra.com>
2025-01-09 16:29:26 +0000

    fix exports

--- index.ts
+++ index.ts
@@ -1,14 +1,22 @@
-import { validateCategoryName } from "./src/validateCategoryName.js";
-import { validateIconName } from "./src/validateIconName.js";
 import {
   ZetaIconNameError,
   RenameErrorType,
   ErrorSeverity,
+  ZetaIconError,
 } from "./src/types.js";
+import { validateCategoryName } from "./src/validateCategoryName.js";
+import { validateIcon } from "./src/validateIcon.js";
+import { validateIconName } from "./src/validateIconName.js";
+import { validateIconSize } from "./src/validateIconSize.js";
+import { validateProperties } from "./src/validateProperties.js";
 
 export {
-  validateIconName,
+  validateIcon,
   validateCategoryName,
+  validateIconName,
+  validateIconSize,
+  validateProperties,
+  ZetaIconError,
   ZetaIconNameError,
   RenameErrorType,
   ErrorSeverity,
